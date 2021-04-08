2b04443a40fcd0d4721d1a460aaccc4879b4cd3f
Dand Nguyen <nguyendanhda98@gmail.com>
2021-04-08 11:32:23 +0700

    function done

--- src/components/workList.js
+++ src/components/workList.js
@@ -2,13 +2,11 @@ import Item from "./item";
 import {useState} from "react";
 function WorkList ( {works, setWorks}) {
 
-
-
     function Delete(id) {
          const newList = works.filter((item) => item.id !== id);
         setWorks(newList);
-    }
 
+    }
 
     return (
         <div>
